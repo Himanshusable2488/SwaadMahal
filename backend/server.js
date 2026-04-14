9799e09362da338c2af320a6f9f53d1af02290f7
Himanshu Sable <himanshusable2488@gmail.com>
2026-04-14 22:57:20 +0530

    fixed port for render

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 app.use('/api/bookings', bookingRoutes);
 app.use('/api/orders',orderRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`);
