317fb972de90f8fb9cfb217a46b17d7514f66b4f
Himanshu Sable <himanshusable2488@gmail.com>
2026-04-14 20:57:43 +0530

    added root route

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Backend is running 🚀');
+});
+
 app.use('/api/bookings', bookingRoutes);
 app.use('/api/orders',orderRoutes);
 
